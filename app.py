c2ed79e50ac3c43bcf14a923996a879acafe833a
Nicole Martinez <nmartinez@argusinvent.com>
2016-11-09 20:42:55 -0600

    added auth route and now you can create user tokens

--- app.py
+++ app.py
@@ -7,12 +7,17 @@ if(curpath not in sys.path):
 
 import falcon
 import users
+import auth
 
 api = application = falcon.API()
 
 db = os.path.join(curpath, 'data.db')
+
 usersManage = users.UserResource(db)
 userCollection = users.UserCollectionResource(db)
 
 api.add_route('/users/{userID}', usersManage)
 api.add_route('/users', userCollection)
+
+authResource = auth.AuthResource(db)
+api.add_route('/auth', authResource)
--- auth.py
+++ auth.py
@@ -9,10 +9,10 @@ class AuthRepository:
     def __init__(self, dbPath):
         self.dbPath = dbPath
 
-    def createAuthToken(self, userID, lifespan):
+    def createToken(self, userID, lifespan):
         conn = sqlite3.connect(self.dbPath);
         c = conn.cursor()
-        query = "INSERT INTO auth_tokens(token, user_id, created, expires) \
+        query = "INSERT INTO auth_token(token, user_id, created, expires) \
                  VALUES(?, ?, ?, ?)"
 
         token = uuid.uuid4().hex
@@ -62,3 +62,39 @@ class AuthRepository:
         conn.close()
 
         return c.rowcount > 0
+    
+class AuthResource(object):
+    def __init__(self, dbPath):
+        self.users = UserRepository(dbPath)
+        self.repository = AuthRepository(dbPath)
+        self.lifespan = 24 * 60 * 60
+        
+    def on_post(self, request, response):
+        if request.content_length:
+            try:
+                raw_data = request.stream.read().decode("utf-8")
+            except Exception as ex:
+                raise falcon.HTTPError(falcon.HTTP_400, 'Bad request',
+                                       'Error reading request body')
+
+            try:
+                authdata = json.loads(raw_data)
+            except ValueError:
+                raise falcon.HTTPError(falcon.HTTP_400, 'Malformed JSON',
+                    'Could not decode request body. JSON was improperly formed.')
+        else:
+            raise falcon.HTTPError(falcon.HTTP_400, 'Empty Request',
+                                   'The request body was empty.')
+        
+        authUser = self.users.checkUser(authdata['username'], authdata['password'])
+
+        if authUser is None:
+            raise falcon.HTTPError(falcon.HTTP_401, "Incorrect Credentials",
+                                   'The username and password were incorrect')
+        
+        token = self.repository.createToken(authUser, self.lifespan)
+
+        response.body = '{"token": "' + token + '"}'
+        response.status = falcon.HTTP_200
+
+        
